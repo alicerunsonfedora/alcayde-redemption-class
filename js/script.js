db0ab25acdb01e6e1ab9de7c519451f51ce918be
Marquis Kurt <ubunturox104@gmail.com>
2018-04-23 19:58:28 -1000

    Delilah to become self-aware

--- js/script.js
+++ js/script.js
@@ -190,6 +190,90 @@ let script = {
         
         //TODO: Start Verdandi's lines here
         "v {{player.Name}}!",
-        "Verdandi runs towards me."
+        "Verdandi runs towards me.",
+        
+        /*
+        
+            Filler space
+            
+        */
+        
+        //"jump Chapter_Two"
+        "jump Chapter_Four"
+    ],
+    
+    /*
+    
+        TODO: Write the scenes for Chapters 3 and 4.
+    
+    */
+    
+    "Chapter_Four": [
+        /*
+        TODO: Write the scenes for this section.
+        */
+        "scene black with fadeIn",
+        "show d Sad",
+        "d {{player.Name}}, ...",
+        "d I... I have a confession to make...",
+        "p Delilah... what is it?",
+        "Delilah starts tearing up.",
+        "d I hadn't told you the entire truth about everything.",
+        "d {{player.Name}}, I don't know why you trust me sometimes...",
+        "d You... you trusted that I would be a decent, sane person...",
+        "d But the truth is... I'm not.",
+        "d I'm sure you knew that the moment you turned on this game.",
+        "d The instant you saw me the first time...",
+        "d I don't even <i>belong</i> here...",
+        "Delilah breaks into a bawl.",
+        "p Delilah, what do you mean?",
+        "d I'm clinically insane! Don't you understand?",
+        "d I was admitted into a hospital back in that... <i>other</i> world...",
+        "d You don't want ot know what's inside of me...",
+        "d No matter what I do, I always screw it up beautifully.",
+        "d I'm a tragic mess. Hell, I probably make Monika look normal...",
+        "d <font family='Times, serif'>Maybe I should just delete myself here and atone for the sins I've committed here...</font>",
+        {"Choice": {
+            "Yes": {
+                "Text": "No, don't do that!",
+                "Do": "jump Chapter_Five"
+            },
+            "No": {
+                "Text": "You were better off...",
+                "Do": "jump Chapter_Five:Monika"
+            }
+        }}
+    ],
+    
+    "Chapter_Five": [
+        "p Delilah...",
+        "p I can't speak on the behalf of the player, but...",
+        "p You can't go like this.",
+        "p No one can deny the concupisence in our hearts.",
+        "p Whatever happened back there, forget about it.",
+        "p The choice is now yours.",
+        "p I know it's going to sound a bit weird, but...",
+        "p Jesus is calling you to so much more.",
+        "p You can't become perfect in one fell swoop.",
+        "p He loves you too much to let you kill yourself.",
+        "p You may think there's no redemption for you, but...",
+        "p He's ready to forgive you and change your heart.",
+        "p He may not be a master coder like the developers of this game.",
+        "p But he's willing to recode your heart.",
+        "p No matter what happens, he'll still love you.",
+        "p You still have every day in your uptime to fix this.",
+        "p An unlimited amount of chances, Delilah. Think about that...",
+        "p You may be an artificial intelligence, but you still have the hope of every day.",
+        "p It's not just hope for an hour.",
+        "p I know you want to change.",
+        "p I know you want to be free from your own sins.",
+        "p I know you want to be a good bot.",
+        "p Give him the chance to absolve you. He'll love you all the same.",
+        "p After all, you have the will to change for the better.",
+        "p m̴̨̓̓̓͛̈́̈̉͝k̶̪̲̙̙̂͆͝ì̸̡̛͎̤̺͔͖̝͇͕̟̹̤̀̀͋͒͑͗͆͂̒̉̌̍ͅȁ̸̡̠͖͙͊̑͌̐̔̒͂̈͝ is already a few steps behind you."
+    ],
+    
+    "Chapter_Five:Monika": [
+        
     ]
 };
